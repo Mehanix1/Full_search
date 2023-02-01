6841a8662224f79c77e0178a03f0c2e6bfd82aef
Mikhail Zimin <you@example.com>
2023-02-01 18:47:33 +0300

    commit

--- geocoder_api.py
+++ geocoder_api.py
@@ -13,22 +13,46 @@ def get_coordinates(address: str) -> tuple[float | None, float | None]:
     return toponym_longitude, toponym_lattitude
 
 
+def get_coordinates_and_span(address: str) -> tuple[tuple[float | None, float | None], tuple[float | None, float | None]]:
+    toponym = geocoder_request(address)
+    if toponym is None:
+        return (None, None), (None, None)
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym['Point']['pos']
+    # Широта, преобразованная в плавающее число:
+    toponym_longitude, toponym_lattitude = map(float, toponym_coodrinates.split(' '))
+
+    # Рамка вокруг объекта:
+    envelope = toponym['boundedBy']['Envelope']
+    # левая, нижняя, правая и верхняя границы из координат углов:
+    l, b = map(float, envelope['lowerCorner'].split())
+    r, t = map(float, envelope['upperCorner'].split())
+    # Вычисляем полуразмеры по вертикали и горизонтали
+    dx = abs(l - r) / 2
+    dy = abs(t - b) / 2
+
+    return (toponym_longitude, toponym_lattitude), (dx, dy)
+
+
 API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
 
 
-def geocoder_request(address: str) -> dict:
+def geocoder_request(address: str) -> dict | None:
+    # Выполняем запрос.
     response = requests.get('http://geocode-maps.yandex.ru/1.x/', params={
         'apikey': API_KEY,
         'geocode': address,
-        'format': 'json'
+        'format': 'json',
     })
+
     if not response:
         raise RuntimeError(
             f'''Ошибка выполнения запроса:
-                {response.request.url}
-                Http статус: {response.status_code} ({response.reason})''')
+            {response.request.url}
+            Http статус: {response.status_code} ({response.reason})''')
 
-        # Преобразуем ответ в json-объект
+    # Преобразуем ответ в json-объект
     data = response.json()
     # Получаем первый топоним из ответа геокодера.
     # Согласно описанию ответа он находится по следующему пути:
